8c0cc80dc0b96f4bbfcc49782a891c9d091b5082
Adidas <hoangngoAZ8799@gmail.com>
2025-09-15 18:42:31 +0700

    Cập nhật api.ts lan2

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,12 @@
 export const login = async (username: string, password: string) => {
-  console.log("Sending login request to:", "http://127.0.0.1:8000/api/token/", {
+  const API_URL = "http://14.224.210.210:8000/api/token/";
+
+  console.log("Sending login request to:", API_URL, {
     username,
     password,
   });
 
-  const response = await fetch("http://127.0.0.1:8000/api/token/", {
+  const response = await fetch(API_URL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password }),
